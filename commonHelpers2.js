f15c2b36f8414e662f218a3bec189397f8b494f7
sofiiahoppova <156512468+sofiiahoppova@users.noreply.github.com>
2024-06-08 15:27:18 +0000

    Deploying to gh-pages from @ sofiiahoppova/goit-js-hw-10@aba112497a7ba4d8550846978eff7f4659a281eb 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.elements.delay.setAttribute("step",1e3);s.addEventListener("submit",e=>{e.preventDefault();const m=s.elements.delay.value,o=s.elements.state.value;n(m,o).then(t=>{i.show({message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{i.show({message:`❌ Rejected promise in ${t}ms`})})});function n(e,m){return new Promise((t,r)=>{setTimeout(()=>{m==="fulfilled"?t(e):r(e)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
